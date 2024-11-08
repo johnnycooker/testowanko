66c86272be03e8805ee4bbda484eeac966087d6d
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-11-08 17:03:33 +0100

    Add src/pages/index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+	return (
+		<>
+			<div className="bg-white w-full h-full text-black">
+				Test Pierwszego deploymentu
+			</div>
+		</>
+	);
+};
+
+export default HomePage;
